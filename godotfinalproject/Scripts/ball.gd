23359b230eed90d59354b482c9a23ee397a2b0b6
KimMinSung235 <alexkim0638@gmail.com>
2024-12-16 16:08:09 +0900

    bbb

--- godotfinalproject/Scripts/ball.gd
+++ godotfinalproject/Scripts/ball.gd
@@ -104,17 +104,8 @@ func _on_area_entered(area: Area2D) -> void:
 			set_physics_process(true)
 			
 func _on_body_entered(body: Node2D) -> void:
-	if !isOnCollider:
-		isOnCollider = true
-		set_physics_process(false)
-		if body.name == "몹":
-			print("몹 건드림")
-			set_physics_process(true)
-
-func _on_body_exited(body: Node2D) -> void:
-	print("탈출")
-	isOnCollider = false		
-
+	if body.name == "몹":
+		print("몹 건드림")
 
 func _on_area_exited(area: Area2D) -> void:
 	print("탈출")
